0d940f74c4e02a0c878a7b0fde5f0c73408c0b74
Danya <danyafrisina@hotmail.it>
2020-04-06 15:41:33 +0200

    frisina_danya_quest020

--- front/src/components/SignUp.js
+++ front/src/components/SignUp.js
@@ -4,21 +4,94 @@ class SignUp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: 'Email:'
+      email: 'Email:',
+      password: 'Password:',
+      passwordConf: 'Confirm password:',
+      firstname: 'First name:',
+      lastname: 'Last Name:',
     };
     this.updateEmailField = this.updateEmailField.bind(this);
+
+    this.updatePasswordField = this.updatePasswordField.bind(this);
+
+    this.updatePasswordConfField = this.updatePasswordConfField.bind(this);
+
+    this.updateFirstnameField = this.updateFirstnameField.bind(this);
+
+    this.updateLastnameField = this.updateLastnameField.bind(this);
   }
 
-  updateEmailField = event => {
+  updateEmailField = (event) => {
     this.setState({ email: event.target.value });
+  };
+
+  updatePasswordField = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  updatePasswordConfField = (event) => {
+    this.setState({ passwordConf: event.target.value });
+  };
+
+  updateFirstnameField = (event) => {
+    this.setState({ firstname: event.target.value });
+  };
+
+  updateLastnameField = (event) => {
+    this.setState({ lastname: event.target.value });
+  };
+
+  // handleSubmit(e) {
+  //   e.preventDefault();
+  //   console.log('This form has been submitted with these fields', this.state);
+  // }
+
+  updateLastnameField = (event) => {
+    this.setState({ lastname: event.target.value });
   };
 
   render() {
     return (
       <div>
-        <h1>{JSON.stringify(this.state, 1, 1)}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <p>{this.state.email}</p>
+          <input name="email" type="email" onChange={this.updateEmailField} />
 
-        <input name="email" type="email" onChange={this.updateEmailField} />
+          <p>{this.state.password}</p>
+          <input
+            name="password"
+            type="text"
+            onChange={this.updatePasswordField}
+          />
+
+          <p>{this.state.passwordConf}</p>
+          <input
+            name="passwordConf"
+            type="text"
+            onChange={this.updatePasswordConfField}
+          />
+
+          <p>{this.state.firstname}</p>
+          <input
+            name="firstname"
+            type="text"
+            onChange={this.updateFirstnameField}
+          />
+
+          <p>{this.state.lastname}</p>
+          <input
+            name="lastname"
+            type="text"
+            onChange={this.updateLastnameField}
+          />
+          <br />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+
+        {/* <h1>{JSON.stringify(this.state, 1, 1)}</h1>
+
+        <input name="email" type="email" onChange={this.updateEmailField} /> */}
       </div>
     );
   }
